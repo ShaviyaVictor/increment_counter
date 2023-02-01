149c373d5ef08ddfedc190a8571e172672e33dce
shaviyavictor <shaviyavictor@gmail.com>
2023-02-01 16:44:34 -0500

    Create and set up the store and provide a provider to enable the access of the state throughout the application

--- src/index.js
+++ src/index.js
@@ -4,12 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import myReducers from './reducers';
+
+// create the store
+const myStore = createStore(myReducers);
+
+// This will console log the current state everytime the state changes
+myStore.subscribe(()=>console.log(myStore.getState()));
+
+// Enveloping the App inside the Provider to ensure that the states in the store are available throughout the application
+ReactDOM.render(
+  <Provider store={myStore}>
     <App />
-  </React.StrictMode>
+  </Provider>, 
+  document.getElementById('root')
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
